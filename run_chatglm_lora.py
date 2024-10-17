f560a67731f779901c111b7ca36443721f157a49
AIR-frank <2609242369@qq.com>
2024-10-17 17:49:47 +0800

    A800也只能开到2

--- run_chatglm_lora.py
+++ run_chatglm_lora.py
@@ -3,7 +3,7 @@ from transformers import AutoTokenizer, AutoConfig
 
 tokenizer = AutoTokenizer.from_pretrained("/root/model/chatglm3-6b", trust_remote_code=True)
 config = AutoConfig.from_pretrained("/root/model/chatglm3-6b", trust_remote_code=True)
-trainer = Trainer(tokenizer=tokenizer, config=config, from_pretrained='/root/model/chatglm3-6b', loader_name='ChatGLM_Chat', data_path='qa_dataset', max_length=3200, batch_size=1, task_name='qa_dataset')
+trainer = Trainer(tokenizer=tokenizer, config=config, from_pretrained='/root/model/chatglm3-6b', loader_name='ChatGLM_Chat', data_path='qa_dataset', max_length=3200, batch_size=2, task_name='qa_dataset')
 
 for i in trainer(num_epochs=60, resume_step=25746, lr=1e-5):
     a = i
